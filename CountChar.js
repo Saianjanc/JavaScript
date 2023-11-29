67bf7d8eb054a49163e5b8f5639642fd0d1c2d06
unknown <saianjan1234@gmail.com>
2023-11-29 16:08:41 +0530

    Added the First Program!1~

--- CountChar.js
+++ CountChar.js
@@ -3,8 +3,6 @@ const readline = require('readline-sync');
 const str = readline.question("Enter the first string: ");
 
 var count = 0
-str1=str.split(" ").join("")
-for (var i=0;i<str1.length;i++){
-    count++
-}
+const str1=str.split(" ").join("")
+count = str1.length()
 console.log(count)
