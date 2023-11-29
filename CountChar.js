16d68b38901ebdc182a95f352857ea88066c265d
unknown <saianjan1234@gmail.com>
2023-11-29 16:10:03 +0530

    Added the First Program!

--- CountChar.js
+++ CountChar.js
@@ -1,8 +1,7 @@
+//Program to count the number of characters in the given string without including spaces. 
 const readline = require('readline-sync');
 
 const str = readline.question("Enter the first string: ");
 
-var count = 0
 const str1=str.split(" ").join("")
-count = str1.length()
-console.log(count)
+console.log(str1.length)
